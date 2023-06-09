83d9431fd1f0adb9c6b3bb95824e0d6be4cb4596
Manuxxo <manucarretec@gmail.com>
2023-06-09 19:33:28 +0200

    a

--- app/src/main/java/mc/tfg_tiempo/ClimaApiCliente.kt
+++ app/src/main/java/mc/tfg_tiempo/ClimaApiCliente.kt
@@ -4,10 +4,10 @@ import okhttp3.*
 import org.json.JSONObject
 import java.io.IOException
 
-class ClimaApiCliente() {
+class ClimaApiCliente(private val apiKey: String) {
 
     private val cliente = OkHttpClient()
-    private val apiKey = "95b1f5bbf3915b3c1c96eaf1e6c348e3"
+
     fun getClimaActual(city: String, callback: respuestaWeather) {
         val url = "https://api.openweathermap.org/data/2.5/weather?q=$city&units=metric&appid=$apiKey"
 
@@ -25,14 +25,17 @@ class ClimaApiCliente() {
             }
         })
     }
+
+
+
     companion object {
         fun parseWeatherData(json: String): DatosWeather {
             val jsonObject = JSONObject(json)
             val mainObject = jsonObject.getJSONObject("main")
             val temperatura = mainObject.getDouble("temp")
             val humedad = mainObject.getInt("humidity")
-            val iconObject = jsonObject.getJSONObject("weather")
-            val icon = iconObject.getString("icon")
+
+            val icon = "04d"
 
             return DatosWeather(temperatura, humedad, icon)
         }
--- app/src/main/java/mc/tfg_tiempo/InformacionClimaFragment.kt
+++ app/src/main/java/mc/tfg_tiempo/InformacionClimaFragment.kt
@@ -1,12 +1,17 @@
 package mc.tfg_tiempo
 
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.squareup.picasso.Picasso
+import com.squareup.picasso.Target
 import mc.tfg_tiempo.databinding.FragmentInformacionClimaBinding
 import okhttp3.Response
+import java.lang.Exception
 
 class InformacionClimaFragment : Fragment() {
 
@@ -21,9 +26,8 @@ class InformacionClimaFragment : Fragment() {
         enlace= FragmentInformacionClimaBinding.inflate(inflater, container, false)
         return enlace.root
     }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        climaApiCliente = ClimaApiCliente()
+        climaApiCliente = ClimaApiCliente("95b1f5bbf3915b3c1c96eaf1e6c348e3")
 
         enlace.buttonGetWeather.setOnClickListener {
             val city = "El Puerto de Santa María"
@@ -33,12 +37,41 @@ class InformacionClimaFragment : Fragment() {
                     val body = response.body?.string()
                     if (response.isSuccessful && !body.isNullOrEmpty()) {
                         val weatherData = ClimaApiCliente.parseWeatherData(body)
-                        val temperature = weatherData.temperatura.toInt()
-                        val humidity = weatherData.humedad
+                        val temperatura = weatherData.temperatura.toInt()
+                        val humedad = weatherData.humedad
+                        val icon = weatherData.icon
+
+                        val iconUrl = "http://openweathermap.org/img/w/$icon.png"
+
+                        //esto es para que trabaje en un hilo diferente y sea más seguro que no crashe
 
                         view.post {
-                            enlace.textViewTemperature.text = "Temperatura: $temperature °C"
-                            enlace.textViewHumidity.text = "Humedad: $humidity %"
+                            enlace.textViewTemperature.text = "Temperatura: $temperatura °C"
+                            enlace.textViewHumidity.text = "Humedad: $humedad %"
+
+                            //Picasso agarra un resource desde una web y lo pone en un elemento
+                            Picasso.get()
+                                .load(iconUrl)
+                                .into(object : Target{
+                                    override fun onBitmapLoaded(
+                                        bitmap: Bitmap?,
+                                        from: Picasso.LoadedFrom?
+                                    ) {
+                                        enlace.imageViewIcon.setImageBitmap(bitmap)
+                                    }
+
+                                    override fun onBitmapFailed(
+                                        e: Exception?,
+                                        errorDrawable: Drawable?
+                                    ) {
+
+                                    }
+
+                                    override fun onPrepareLoad(placeHolderDrawable: Drawable?) {
+
+                                    }
+
+                                })
                         }
                     } else {
                         onError()
